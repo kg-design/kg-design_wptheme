791cabc878ed72eae41c695c57ba9b38a120f069
kg-design <karen.grigoryan@live.com>
2016-10-02 16:56:36 +0100

    function.php footer menu change 2

--- functions.php
+++ functions.php
@@ -45,7 +45,7 @@ function kgdesign_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary', 'kgdesign' ),
-		'footer'  => esc_html__( 'Footer-kg', 'kgdesign' ),
+		'footer'  => esc_html__( 'Footer', 'kgdesign' ),
 
 	) );
 
